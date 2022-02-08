d2e7624c5bf94309f7efc475788a3088edf60d34
Danill Lautenshleger <den2033@bk.ru>
2022-02-08 22:51:33 +0300

    Third commit. CircleList and Two-ConnectedList has been added to project.

---
+++ Data_structures/CircleList/Model/CircleList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleList.Model
+{
+    public class CircleList<T>
+    {
+        public int Count { get; private set; }
+        public Item<T> Head { get; set; }
+
+        public Item<T> Tail { get; set; }
+
+
+        public CircleList()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+
+        public CircleList(T data)
+        {
+            var item = new Item<T>(data);
+            Head = item;
+            Tail = item;
+            Count = 1;
+        }
+
+        public void Add(T data)
+        {
+            var item = new Item<T>(data);
+            if (Tail != null)
+            {
+                Tail.Next = item;
+                Tail = item;
+                Tail.Next = Head;
+                Count++;
+            }
+            else
+            {
+                Head = item;
+                Tail = item;
+                Count = 1;
+            }
+        }
+
+        public void Remove(T data)
+        {
+            if (Head != null)
+            {
+
+                if (Head.Data.Equals(data))
+                {
+                    Tail.Next = Head.Next;
+                    Head = Head.Next;
+                    Count--;
+                }
+                else
+                {
+                    var previous = Head;
+                    var current = Head.Next;
+                    while (!current.Data.Equals(data))
+                    {
+                        previous = current;
+                        current = current.Next;
+                    }
+                    if (current == Tail)
+                        Tail = previous;
+                    previous.Next = current.Next;
+                    Count--;
+                }
+            }
+            else
+            {
+                throw new NullReferenceException("List is empty");
+            }
+        }
+    }
+}
---
+++ Data_structures/TwoConnectedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using TwoConnectedList.Model;
+namespace Data_structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Two_WayLinkedList<int> list = new Two_WayLinkedList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+
+
+            list.Remove(1);
+            list.Remove(3);
+
+            Console.ReadLine();
+        }
+    }
+}
