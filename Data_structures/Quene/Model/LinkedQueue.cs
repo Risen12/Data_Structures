b93e4934e2224823c4f13b92c8d19c2006f79ab8
Danill Lautenshleger <den2033@bk.ru>
2022-02-14 14:18:15 +0300

    Queue,EasyQueue by list, ArrayQueue by array and Two-WayLinkedQueue and LinkedQueue has been added to the project.

---
+++ Data_structures/Quene/Model/LinkedQueue.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+
+namespace Queue.Model
+{
+    public class LinkedQueue<T> :IEnumerable
+    {
+        public LinkedItem<T> Head { get; set; }
+        public LinkedItem<T> Tail { get; set; }
+
+        public int Count { get; set; }
+
+        public LinkedQueue() { }
+
+        public LinkedQueue(T data)
+        {
+            SetHead(data);
+        }
+        private void SetHead(T data)
+        {
+            var item = new LinkedItem<T>(data);
+            Head = item;
+            Tail = item;
+            Count = 1;
+        }
+
+        public void Enqueue(T data)
+        {
+            if (Head == null)
+            {
+                SetHead((T)data);
+                return;
+            }
+
+            var item = new LinkedItem<T>(data);
+            item.Next = Tail;
+            Tail = item;
+            Count++;
+        }
+
+        public T Decqueue()
+        {
+            var result = Head.Data;
+            if (Count == 1)
+            {
+                Head = null;
+                Tail = null;
+                Count--;
+                return result;
+            }
+
+            var current = Tail.Next;
+            var previous = Tail;
+            while (current != Head)
+            { 
+                previous = current;
+                current = current.Next;
+            }
+            previous.Next = null;
+            Head = previous;
+            Count--;
+            return result;
+        }
+
+        public T Peek()
+        { 
+            return Head.Data;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            var current = Tail;
+            while (current != null)
+            { 
+                yield return current;
+                current = current.Next;
+            }
+        }
+    }
+}
---
+++ Data_structures/Quene/Program.cs
@@ -0,0 +1,131 @@
+﻿
+
+using Quene.Model;
+using Queue.Model;
+
+namespace Queue // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            LinkedQueue<int> linkedQueue = new LinkedQueue<int>();
+            linkedQueue.Enqueue(1);
+            linkedQueue.Enqueue(2);
+            linkedQueue.Enqueue(3);
+            linkedQueue.Enqueue(4);
+            linkedQueue.Enqueue(5);
+
+            foreach (var item in linkedQueue)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(linkedQueue.Decqueue());
+            Console.WriteLine(linkedQueue.Peek());
+            Console.WriteLine(linkedQueue.Decqueue());
+            Console.WriteLine(linkedQueue.Peek());
+
+            Console.WriteLine();
+
+            foreach (var item in linkedQueue)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+
+
+            // ------------------------------------------
+
+            //Linked Queue 
+
+            var queue = new TwoWayLinkedQueue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            // ------------------------------------------
+
+            //EasyQueue by List
+
+
+
+            var easyqueue = new EasyQueue<int>();
+            easyqueue.Enqueue(1);
+            easyqueue.Enqueue(2);
+            easyqueue.Enqueue(3);
+            easyqueue.Enqueue(4);
+            easyqueue.Enqueue(5);
+
+            easyqueue.items.ForEach(item => Console.WriteLine(item));
+
+            Console.WriteLine();
+
+            easyqueue.Dequeue();
+            easyqueue.Dequeue();
+            easyqueue.Dequeue();
+
+            easyqueue.items.ForEach(item => Console.WriteLine(item));
+
+
+
+
+            ArrayQueue<int> Arrayqueue = new ArrayQueue<int>(5);
+            Arrayqueue.Enqueue(1);
+            Arrayqueue.Enqueue(2);
+            Arrayqueue.Enqueue(3);
+            Arrayqueue.Enqueue(4);
+            Arrayqueue.Enqueue(5);
+
+            for (int i = 0; i < Arrayqueue.Count; i++)
+            {
+                Console.WriteLine(Arrayqueue.Items[i]);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(Arrayqueue.Decqueue());
+
+            Console.WriteLine();
+
+            for (int i = 0; i < Arrayqueue.Count; i++)
+            {
+                Console.WriteLine(Arrayqueue.Items[i]);
+            }
+
+            Console.WriteLine();
+
+            Arrayqueue.Enqueue(1);
+            Arrayqueue.Enqueue(7);
+            Arrayqueue.Enqueue(3);
+            for (int i = 0; i < Arrayqueue.Count; i++)
+            {
+                Console.WriteLine(Arrayqueue.Items[i]);
+            }
+
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
